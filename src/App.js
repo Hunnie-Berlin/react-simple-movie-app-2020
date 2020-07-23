1617c229dda08dd47fd076fbca431fac8de29ecb
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-07-23 23:54:30 +0200

    Data fetching and redering title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from './Movies';
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=year");
+    this.setState({ movies, isLoading: false });
+  };
+
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+            <Movie
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              rating={movie.rating}
+            />
+          ))}
+      </div >
+
+    );
+  }
 }
+
+
 
 export default App;
